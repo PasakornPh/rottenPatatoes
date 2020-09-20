678fe5129204f38500d228cb0616d60b35794414
PasakornPh <s6101012630135@email.kmutnb.ac.th>
2020-09-20 11:31:36 -0700

    Update before use API twitter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,8 +21,13 @@ class MoviesController < ApplicationController
         params.require(:movie)
         permitted = params[:movie].permit(:title,:rating,:release_date)
         @movie = Movie.create!(permitted)
-        flash[:notice] = "#{@movie.title} was successfully created."
-        redirect_to movies_path
+
+        if @movie.save
+            flash[:notice] = "#{@movie.title} was successfully created."
+            redirect_to movies_path
+        else
+            render 'new' # note, 'new' template can access @movie's field values!
+        end
     end
 
     def edit
@@ -32,10 +37,12 @@ class MoviesController < ApplicationController
     def update
         @movie = Movie.find params[:id]
         permitted = params[:movie].permit(:title,:rating,:release_date)
-        @movie.update_attributes!(permitted)
-         
-        flash[:notice] = "#{@movie.title} was successfully updated."
-        redirect_to movie_path(@movie)
+        if @movie.update_attributes!(permitted)
+            flash[:notice] = "#{@movie.title} was successfully updated."
+            redirect_to movie_path(@movie)
+        else
+            render 'edit' # note, 'edit' template can access @movie's field values!
+        end
     end
 
     def destroy
@@ -45,3 +52,10 @@ class MoviesController < ApplicationController
         redirect_to movies_path
     end
 end
+
+
+  # replaces the 'update' method in controller:
+  def update
+    @movie = Movie.find params[:id]
+    
+  end
