350cfbdc08f503a03e097832095f75b4fe70c447
PasakornPh <s6101012630135@email.kmutnb.ac.th>
2020-10-10 09:01:00 -0700

    Add Ajax popup and check box

---
+++ public/packs/js/movie_popup-f7f22962b565c9304996.js
@@ -0,0 +1,139 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/movie_popup.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/packs/movie_popup.js":
+/*!*********************************************!*\
+  !*** ./app/javascript/packs/movie_popup.js ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function($) {var MoviePopup = {
+  setup: function setup() {
+    // add hidden 'div' to end of page to display popup:
+    var popupDiv = $('<div id="movieInfo"></div>');
+    popupDiv.hide().appendTo($('body'));
+    $(document).on('click', '#More', MoviePopup.getMovieInfo);
+  },
+  getMovieInfo: function getMovieInfo() {
+    $.ajax({
+      type: 'GET',
+      url: $(this).attr('href'),
+      timeout: 5000,
+      success: MoviePopup.showMovieInfo,
+      error: function error(xhrObj, textStatus, exception) {
+        alert('Error!');
+      } // 'success' and 'error' functions will be passed 3 args
+
+    });
+    return false;
+  },
+  showMovieInfo: function showMovieInfo(data, requestStatus, xhrObject) {
+    // center a floater 1/2 as wide and 1/4 as tall as screen
+    var oneFourth = Math.ceil($(window).width() / 4);
+    $('#movieInfo').css({
+      'left': oneFourth,
+      'width': 2 * oneFourth,
+      'top': 250
+    }).html(data).show(); // make the Close link in the hidden element work
+
+    $('#closeLink').click(MoviePopup.hideMovieInfo);
+    return false; // prevent default link action
+  },
+  hideMovieInfo: function hideMovieInfo() {
+    $('#movieInfo').hide();
+    return false;
+  }
+};
+$(MoviePopup.setup);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module 'jquery'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()))))
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=movie_popup-f7f22962b565c9304996.js.map
