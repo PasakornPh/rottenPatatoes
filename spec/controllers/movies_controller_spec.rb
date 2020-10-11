a0447ea4de5c6ba7be83fe32c3e8df4760879cea
PasakornPh <s6101012630135@email.kmutnb.ac.th>
2020-10-11 09:14:05 -0700

    Add Rspec TDD 2 Create movie

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -11,6 +11,7 @@ describe MoviesController do
       #post :search_tmdb, {:search_terms => 'hardware'}
       post :search_tmdb, params: {:search_terms => 'hardware'}
     end
+
     describe 'after valid search' do
       before :each do
         allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
@@ -24,5 +25,30 @@ describe MoviesController do
         expect(assigns(:movies)).to eq(@fake_results)
       end
     end
+  end
+
+  describe "#Create" do
+    before (:each) do
+      @mock_movie_attributes = {movie: {title: "new movie",rating: "PG",release_date: "2016-10-01",description:  "new description"}}
+
+    end
+    describe "POST #create" do
+      
+        it "saves the new movie in the database" do
+            expect {
+              post :create,params: @mock_movie_attributes
+            }.to change(Movie,:count).by(1)
+        end
+        it "assigns the saved movie to @movie" do
+            post :create,params: @mock_movie_attributes
+            expect(assigns(:movie).title).to include("New Movie")
+        end
+        it "redirects to the home page" do
+            post :create,params: @mock_movie_attributes
+            expect(response).to redirect_to(movie_path(Movie.last.id))
+        end
+    end
+
+    
   end
 end
